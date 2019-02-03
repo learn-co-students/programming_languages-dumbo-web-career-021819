bf53fecf32694d8a69015e919bf894b5faf3ff77
adisagir <adisagir@gmail.com>
2019-02-02 23:11:42 -0500

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,17 @@
+require 'pry'
 def reformat_languages(languages)
   # your code here
+  new_hash = {}
+  languages.each do |style, name|
+    name.each do |code, info|
+      info.each do |type, value|
+        if new_hash.key?(code) == false
+          new_hash[code] = { type => value, :style => [style] }
+        else
+          new_hash[code][:style] << style
+        end
+      end
+    end
+  end
+  new_hash
 end
