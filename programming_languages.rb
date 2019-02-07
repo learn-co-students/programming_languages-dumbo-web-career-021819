715f787e2fa15c6fa52edce1a796a1827e6db145
Antoinette Lee <antoinette.lee02@myhunter.cuny.edu>
2019-02-07 01:51:12 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,22 @@
 def reformat_languages(languages)
-  # your code here
+  languages_new = {}
+  languages.each do |styles, names|
+    names.each do |name, type|
+      type.each do |key, value|
+        languages_new[name] = {:type => nil, :style => []}
+        languages_new[name][:type] = value
+      end
+    end
+  end
+  languages.each do |styles, names|
+    names.each do |name, type|
+      languages_new[name][:style] << styles
+    end
+  end
+  languages_new
 end
+
+
+
+
+
